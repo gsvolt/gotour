4aa1384948e0c4a7836c34909d67730368e96055
gsvolt <gsvolt7@gmail.com>
2019-03-05 08:12:10 -0500

    added imports.go: factored import statement

---
+++ Module2_Basic/imports.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+// can also do imports on individual lines like import "modulename", but the method above is preferred.
+
+func main() {
+	fmt.Printf("Now you have %g problems.\n", math.Sqrt(7))
+}
