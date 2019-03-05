faea42040c16e1f3df769a36b1206d4aeb30d1aa
gsvolt <gsvolt7@gmail.com>
2019-03-05 13:23:15 -0500

    added type-inference.go: if type is not specified, go tries to figure type out

---
+++ Module2_Basic/type-inference.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main(){
+    v := 42
+    w := 4.2
+    x := "hello"
+    y := 42i
+    fmt.Printf("v is of type %T\nw is of type %T\nx is of type %T\ny is of type %T", v, w, x, y)
+}
