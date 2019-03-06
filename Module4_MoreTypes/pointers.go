38d1195f2ab95cb71534f2874aff418606cc47b1
gsvolt <gsvolt7@gmail.com>
2019-03-06 10:39:09 -0500

    pointers.go: simple pointer example; I added a var and print its address

---
+++ Module4_MoreTypes/pointers.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+
+    i, j := 42, 2701
+
+    p := &i
+    fmt.Println(*p)
+    *p = 21
+    fmt.Println(i)
+
+    p = &j
+    *p = *p / 37
+    fmt.Println(j)
+
+    var k int
+    q := &k
+    fmt.Println(q)
+}
+
