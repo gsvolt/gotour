e6fb91074a3317ced1f7281501805faa65788190
gsvolt <gsvolt7@gmail.com>
2019-05-02 09:58:21 -0400

    added comment

--- Module5_MethodsInterfaces/methods/methods_continued.go
+++ Module5_MethodsInterfaces/methods/methods_continued.go
@@ -2,6 +2,7 @@ package main
 
 type MyFloat float64
 
+// MyAbs Receive would not have worked if MyFloat was in a different package
 func (f MyFloat) MyAbs() float64 {
 	if f < 0 {
 		return float64(-f)
